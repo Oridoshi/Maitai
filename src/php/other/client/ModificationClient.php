6ee6385139ec89e2d08b718442bcbca07a6d5d4a
VicenteHugo <hugovicente965@gmail.com>
2024-04-19 12:56:26 +0200

    correction de la modification du nom

--- src/php/other/client/ModificationClient.php
+++ src/php/other/client/ModificationClient.php
@@ -3,12 +3,15 @@
 include_once '../inc/DB.inc.php';
 
 // initialisations des variables à null
+$prevNomClub = null;
 $nomClub = null;
 $email = null;
 $telephone = null;
 $present = null;
 
 // récupération des données du formulaire si elles sont renseignées
+if(isset($_POST['prevNomClub'])) {$prevNomClub = $_POST['prevNomClub'];}
+
 if(isset($_POST['nomClub'])) {$nomClub = $_POST['nomClub'];}
 
 if(isset($_POST['email'])) {$email = $_POST['email'];}
@@ -21,7 +24,7 @@ if(isset($_POST['present'])) {$present = $_POST['present'];}
 $pdo = DB::getInstance();
 
 // récupération du client à modifier
-$client = $pdo->getClient($nomClub)[0];
+$client = $pdo->getClient($prevNomClub)[0];
 
 // modification des données du client si non null
 if($nomClub != null)
