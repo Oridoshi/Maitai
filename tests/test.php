d0dab28e48bfd5f2302fcbde1d3c76a9065be92c
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2024-04-23 21:21:19 +0200

    ff

--- tests/test.php
+++
@@ -1,7 +0,0 @@
-<h1>Formulaire de Obtention de produits</h1><br><br><br>
-<form action="../src/php/other/produit/GetProduit.php" method="POST">
-	<label for="categ">categ :</label>
-	<input type="categ" id="categ" name="categ"><br><br>
-	
-	<input type="submit" value="Get produits">
-</form>
