535295a5dbe429b79b1790c5b0a843af8414806d
VicenteHugo <hugovicente965@gmail.com>
2024-04-23 15:18:37 +0200

    test effectué

--- tests/test.php
+++ tests/test.php
@@ -1,7 +1,7 @@
 <h1>Formulaire de Obtention de produits</h1><br><br><br>
 <form action="../src/php/other/produit/GetProduit.php" method="POST">
-	<label for="categorie">categorie :</label>
-	<input type="categorie" id="categorie" name="categorie"><br><br>
+	<label for="categ">categ :</label>
+	<input type="categ" id="categ" name="categ"><br><br>
 	
 	<input type="submit" value="Get produits">
 </form>
