579d37ebe855b48ca92d8f37c48549208faccf51
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2024-04-23 21:23:10 +0200

    ff

--- tests/test.php
+++
@@ -1,7 +0,0 @@
-<h1>Formulaire de Suppression de produits</h1><br><br><br>
-<form action="../src/php/other/produit/SuppressionProduit.php" method="POST">
-	<label for="idProd">idProd :</label>
-	<input type="text" id="idProd" name="idProd" required><br><br>
-	
-	<input type="submit" value="Supp produits">
-</form>
