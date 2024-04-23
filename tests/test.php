85f08d8b13d790adc2252e0c26390b2df2411108
VicenteHugo <hugovicente965@gmail.com>
2024-04-23 10:40:04 +0200

    màj test

---
+++ tests/test.php
@@ -0,0 +1,9 @@
+<form action="../src/php/other/clien/CreationClient.php" method="post">
+    <label for="nomClub">Nom du club</label>
+    <input type="text" name="nomClub" id="nomClub" required>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" required>
+    <label for="telephone">Téléphone</label>
+    <input type="tel" name="telephone" id="telephone" required>
+    <input type="submit" value="Créer le client">
+</form>
