93bc5587eac1b5b95b7b3ca86b4b8d040c4cfa42
VicenteHugo <hugovicente965@gmail.com>
2024-04-23 13:47:23 +0200

    petit push

--- src/php/other/client/GetClients.php
+++ src/php/other/client/GetClients.php
@@ -2,6 +2,11 @@
 
 include_once "../inc/DB.inc.php";
 
-$clients = DB::getInstance()->getClients();
+$present = isset($_POST['present']) ? $_POST['present'] : false;
+
+if($present)
+    $clients = DB::getInstance()->getClientsPresent();
+else
+    $clients = DB::getInstance()->getClients();
 
 echo json_encode($clients);
---
+++ tests/test.php
@@ -0,0 +1,4 @@
+<form action="../src/php/other/client/GetClients.php" method="post">
+    <input type="checkbox" name="present" value="1">
+    <input type="submit" value="Get Clients">
+</form>
