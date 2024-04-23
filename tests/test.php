2340242beb0cd236b0b52557bb7e28b20dcc024b
VicenteHugo <hugovicente965@gmail.com>
2024-04-23 10:10:31 +0200

    màj test

---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<form action='../src/php/other/utilisateur/SuppressionUtilisateur.php' method='post'>
+  <label for='login'>Login:</label><br>
+  <input type='text' id='login' name='login' value='Admin'><br>
+  <input type='submit' value='Supprimer'>
+  </form>"; 
