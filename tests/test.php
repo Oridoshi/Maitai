93d533d269b5573b244995fcced488bad09bcb52
VicenteHugo <hugovicente965@gmail.com>
2024-04-23 11:03:23 +0200

    màj test

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,14 @@
-<form action="../src/php/other/clien/CreationClient.php" method="post">
-    <label for="nomClub">Nom du club</label>
-    <input type="text" name="nomClub" id="nomClub" required>
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" required>
-    <label for="telephone">Téléphone</label>
-    <input type="tel" name="telephone" id="telephone" required>
-    <input type="submit" value="Créer le client">
-</form>
+<?php
+
+include '../src/php/inc/Client.inc.php';
+
+$test = new Client();
+$test->setIdCli(0); // Modifier l'id du client pour voir le changement
+$test->setNomClub("club1"); // Modifier le nom du club pour voir le changement (mettre le texte entres les "")
+$test->setTelephone("0606060606"); // Modifier le téléphone pour voir le changement (mettre le texte entres les "")
+$test->setEmail("club1@gmail.com"); // Modifier l'email pour voir le changement (mettre le texte entres les "")
+
+echo 'Id du client : ' . $test->getIdCli() . '<br>';
+echo 'Nom du club : ' . $test->getNomClub() . '<br>';
+echo 'Téléphone : ' . $test->getTelephone() . '<br>';
+echo 'Email : ' . $test->getEmail() . '<br>';
