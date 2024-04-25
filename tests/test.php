127467e4d93a812eb33d09bd7633145e97fd1b7e
Richard-becon <rich.metayer@gmail.com>
2024-04-25 10:23:15 +0200

    près

--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,5 @@
 <h1>Get Produit du Ticket en fonction de client</h1>
-<form action="../src/php/other/ticket/CreationTicket.php" method="post">
+<form action="../src/php/other/ticket/GetTicket.php" method="post">
 
 	<label for="idcli">idcli:</label>
 	<input type="idcli" name="idcli" id="idcli" required><br>
@@ -8,7 +8,7 @@
 </form>
 
 <h1>Get Produit des Ticket</h1>
-<form action="../src/php/other/ticket/CreationTicket.php" method="post">
+<form action="../src/php/other/ticket/GetTicket.php" method="post">
 
 	<input type="submit" value="Envoyer">
 </form>
