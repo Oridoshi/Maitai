be623a21dbabf373bf720a831b13d6e667d7dca5
Richard-becon <rich.metayer@gmail.com>
2024-04-30 13:43:00 +0200

    Ajout des class php pour géré les produit

---
+++ src/php/other/produit/ModificationProduit.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include_once '../../inc/DB.inc.php';
+
+/**
+ * Vérifie si les données POST sont bien définies
+ */
+if(!isset($_POST['idProd']) || !isset($_POST['libProd']) || !isset($_POST['prixUni']) || !isset($_POST['categorie'])) exit;
+
+/**
+ * Crée un nouveau produit avec les données POST
+ */
+$Prod = new Produit();
+$Prod->setIdProd($_POST['idProd']);
+$Prod->setLibProd($_POST['libProd']);
+if($_POST['prixUni'] != ""){
+    $Prod->setPrixUni($_POST['prixUni']);
+} 
+
+$Prod->setCategorie($_POST['categorie']);
+
+/**
+ * Modifie le produit avec le même ID dans la base de données
+ */
+DB::getInstance()->updateProduit($Prod);
