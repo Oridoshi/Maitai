ec72721c7da2f42f1739c455cc095d3f151aaa17
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2024-04-30 10:50:14 +0200

    merge de produit vers la branche

--- src/pages/Produits.js
+++ src/pages/Produits.js
@@ -1,8 +1,310 @@
+import React, { useState, useEffect } from 'react'; // Importez useState ici
+import Table from '../components/Table';
+import { cheminPHP } from '../components/VarGlobal.js';  
 
 export default function produits(){
-	return(
-		<div className="container">
-			<h1>C'est un produit</h1>
+	const [initialData , setInitialData ] = useState([]);
+	const [filterData  , setFilterData  ] = useState([]);
+
+
+
+
+	useEffect(() => {
+		fetch(cheminPHP + "client/GetClients.php", {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'text/plain; charset=UTF-8' // Spécifiez l'encodage ici
+			},
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Erreur de réseau !');
+			}
+			return response.json();
+		})
+		.then(data => {
+			const newData = data.map((item, index) => ({
+				...item,
+				id: index + 1
+			}));
+			setInitialData(newData);
+			setFilterData (newData);
+		})
+		.catch(error => {
+			console.error('Erreur :', error);
+		});
+	}, []);
+
+
+	/*
+		idProd    INTEGER       PRIMARY KEY,
+		libProd   VARCHAR(255)  NOT NULL   ,
+		prixUni   DECIMAL(12,2)            ,
+		categorie VARCHAR(30)   NOT NULL
+	*/
+	// En-tête de la table
+	const initialHeader = [
+		{ id: 'id'       , name: 'NB Ligne'             , type:'number'  , required : true , editable : false, show : false                     },
+		{ id: 'nomclub'  , name: 'Nom du Club'          , type:'text'    , required : true , editable : true , show : true                      },
+		{ id: 'telephone', name: 'Numero de téléphone'  , type:'tel'     , required : true , editable : true , show : true                      },
+		{ id: 'email'    , name: 'Email'                , type:'email'   , required : true , editable : true , show : true                      },
+		{ id: 'present'  , name: 'Présent sur le site'  , type:'checkbox', required : true , editable : true , show : true, fastEditable : true },
+	];
+
+
+
+
+	// Fonction pour l'insertion
+	const funInsert = async (nouvItem) => {
+		try {
+
+
+			const formData = new FormData();
+			formData.append('nomClub'  , nouvItem.nomclub);
+			formData.append('email'    , nouvItem.email);
+			formData.append('telephone', nouvItem.telephone);
+
+
+			if (nouvItem.present) formData.append('present', 1);
+			else                  formData.append('present', 0);
+
+
+			/*
+				$client->setNomClub  ($_POST['nomClub']);
+				$client->setEmail    ($_POST['email']);
+				$client->setTelephone($_POST['telephone']);
+				$client->setPresent  ($_POST['present']);
+			*/
+
+			const requestOptions = {
+				method: 'POST',
+				body: formData
+			};
+
+			const response = await fetch(cheminPHP + "client/CreationClient.php", requestOptions);
+
+			if (!response.ok) {
+				throw new Error('Une erreur s\'est produite.');
+			}
+
+			const data = await response.text();
+			afficherError(data);
+
+			// Récupérer les nouvelles données des clients après l'insertion réussie
+			const newData = await fetchClientData();
+			setInitialData(newData);
+			setFilterData(newData);
+
+			return data === ""; // Retourne true si la suppression a réussi, sinon false
+		} catch (error) {
+			console.log(error);
+			return false; // Retourne false en cas d'erreur
+		}
+
+		// TODO : Regenerer les data avec un fetch
+	};
+
+
+	// Fonction pour l'update
+	const funUpdate = async (nouvItem, oldItem) => {
+		try {
+
+
+			const formData = new FormData();
+			formData.append('prevNomClub', oldItem.nomclub   );
+			formData.append('nomClub'    , nouvItem.nomclub  );
+			formData.append('email'      , nouvItem.email    );
+			formData.append('telephone'  , nouvItem.telephone);
+
+			if (nouvItem.present) formData.append('present', 1);
+			else                  formData.append('present', 0);
+
+			/*
+				$prevNomClub = $_POST['prevNomClub'];
+				$nomClub = $_POST['nomClub'];
+				$email = $_POST['email'];
+				$telephone = $_POST['telephone'];
+				$present = $_POST['present'];
+			 */
+
+			const requestOptions = {
+				method: 'POST',
+				body: formData
+			};
+
+			const response = await fetch(cheminPHP + "client/ModificationClient.php", requestOptions);
+
+			if (!response.ok) {
+				throw new Error('Une erreur s\'est produite.');
+			}
+
+			const data = await response.text();
+			afficherError(data);
+
+			// Récupérer les nouvelles données des clients après l'insertion réussie
+			const newData = await fetchClientData();
+			setInitialData(newData);
+			setFilterData(newData);
+
+			return data === ""; // Retourne true si la suppression a réussi, sinon false
+		} catch (error) {
+			console.log(error);
+			return false; // Retourne false en cas d'erreur
+		}
+	};
+
+
+	// Fonction pour récupérer les données des clients
+	const fetchClientData = async () => {
+		try {
+			const response = await fetch(cheminPHP + "client/GetClients.php", {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'text/plain; charset=UTF-8'
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error('Erreur de réseau lors de la récupération des données des clients.');
+			}
+
+			const data = await response.json();
+			return data.map((item, index) => ({
+				...item,
+				id: index + 1
+			}));
+		} catch (error) {
+			console.error('Erreur :', error);
+			return [];
+		}
+	};
+
+
+	const presentFalseAll = async () => {
+		try {
+			const response = await fetch(cheminPHP + "client/DesactiverClients.php");
+
+			if (!response.ok) {
+				throw new Error('Une erreur s\'est produite.');
+			}
+
+			// Requête pour obtenir les données mises à jour après la désactivation de tous les clients
+			const newDataResponse = await fetch(cheminPHP + "client/GetClients.php", {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'text/plain; charset=UTF-8'
+				},
+			});
+
+			if (!newDataResponse.ok) {
+				throw new Error('Erreur lors de la récupération des données mises à jour.');
+			}
+
+			const newData = await newDataResponse.json();
+
+			// Mettre à jour les données initiales et filtrées avec les nouvelles données
+			const updatedData = newData.map((item, index) => ({
+				...item,
+				id: index + 1
+			}));
+			setInitialData(updatedData);
+			setFilterData(updatedData);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+
+
+	function afficherError(data) {
+		const regex = /SQLSTATE\[(\d+)\].+?(\d+)(.+?) in C:\\xampp/g; // Expression régulière pour capturer le code d'erreur et le texte jusqu'à "in C:\\xampp..."
+		const match = regex.exec(data);
+
+		if (match) {
+			const sqlState = match[1]; // État SQL
+			const errorCode = match[2]; // Code d'erreur
+			const errorMessageText = match[3].trim(); // Texte du message d'erreur
+
+			console.log("Refuse de la base de donnée, raison : ", errorMessageText, "( SQL STATE[", sqlState,"] error code :", errorCode);
+			alert(errorMessageText);
+			
+		} else {
+			if (data !== "")
+				alert(data.replace('<br>', ''));
+		}
+	}
+
+
+
+
+	const handleChange   = (e) => {filter( e.target.value);};
+	const handleCbChange = (e) => {filter( e.target.checked);};
+
+
+	function filter (value)
+	{
+		// Filtrer les données en fonction de la valeur de recherche
+		const filteredData = initialData.filter((element) => {
+
+			if (typeof value === 'boolean')
+			{
+				if(element.present === value || element.present) return true;
+				else                          return false;
+			}
+			else
+			{
+				// Parcourir les clés de l'en-tête initial
+				for (const key of initialHeader) {
+					// Vérifier si la clé doit être affichée et si la valeur de l'élément correspond à la valeur de recherche
+					if (key.show) {
+						// Vérifier si la valeur de l'élément correspond à la valeur de recherche
+						if ((element[key.id] +'').toUpperCase().includes(value.toUpperCase())) {
+							return true; // Si correspondance, conserver cet élément
+						}
+					}
+				}
+				return false; // Si aucune correspondance, exclure cet élément
+			}
+		});
+
+		// Mettre à jour les données filtrées
+		setFilterData(filteredData);
+	}
+
+
+
+
+	//Création du tableau
+	return (
+	<div className="col-sm-12">
+	
+		<h1 className='titre mt-1'>Gestion des clients </h1>
+
+		<div className="grpRecherche mt-4 d-flex align-items-center">
+			{/* barre de recherche */}
+			<div className="col-sm-3">
+				<input className="barre form-control me-2" type="search" placeholder="Rechercher" aria-label="Search" onChange={handleChange} />
+			</div>
+
+			{/* Bouton décoché */}
+			<button className='btn-primary btn mx-2' onClick={presentFalseAll}>Tous décoché</button>
+
+			{/* Bouton checkbox avec style CSS pour la marge gauche */}
+			<div className="form-check" style={{ marginLeft: '10em' }}>
+				<input type='checkbox' className="form-check-input border-secondary" id="afficherClients" onChange={handleCbChange}/>
+				<label className="form-check-label" htmlFor="afficherClients">Afficher seulement clients présents</label>
+			</div>
 		</div>
-	)
+
+
+
+		<Table 
+			header={initialHeader} 
+			data={filterData} 
+			funInsert={funInsert} 
+			funUpdate={funUpdate} 
+			keyGrayWhenFalse = 'present'
+		/>
+	</div>
+	);
 }
--- src/php/inc/DB.inc.php
+++ src/php/inc/DB.inc.php
@@ -1,189 +1,11 @@
 <?php
 
 //Mettre les objet a require ici /!\
-include_once 'Produit.inc.php';
-
-class DB {
-
-	private static $instance = null; //mémorisation de l'instance de DB pour appliquer le pattern Singleton
-	private $connect=null; //connexion PDO à la base
-
-	private static string $dbName   = "maitai";
-	private static string $login    = "Admin";
-	private static string $password = "maitai";
-	private static string $port     = "3306";
-	private static string $host     = "localhost";
-
-
-
-	/************************************************************************/
-	//	Constructeur gerant  la connexion à la base via PDO
-	//	NB : il est non utilisable a l'exterieur de la classe DB
-	/************************************************************************/	
-	private function __construct() {
-
-		// Connexion à la base de données
-		try {
-			// Connexion à la base
-
-			$this->connect = new PDO("mysql:host=" . self::$host . ";port=" . self::$port . ";dbname=" . self::$dbName, self::$login, self::$password);
-
-			// Configuration facultative de la connexion
-			$this->connect->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); 
-			$this->connect->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION); 
-		}
-		catch (PDOException $e) {
-					echo "probleme de connexion :".$e->getMessage();
-			return null;
-		}
-	}
-
-	/************************************************************************/
-	//	Methode permettant d'obtenir un objet instance de DB
-	//	NB : cet objet est unique pour l'exécution d'un même script PHP
-	//	NB2: c'est une methode de classe.
-	/************************************************************************/
-	public static function getInstance() {
-			if (is_null(self::$instance)) {
-			try { 
-			self::$instance = new DB(); 
-		} 
-		catch (PDOException $e) {
-			echo $e;
-		}
-			} //fin IF
-		$obj = self::$instance;
-
-		if (($obj->connect) == null) {
-		self::$instance=null;
-		}
-		return self::$instance;
-	} //fin getInstance	 
-
-	/************************************************************************/
-	//	Methode permettant de fermer la connexion a la base de données
-	/************************************************************************/
-	public function close() {
-			$this->connect = null;
-	}
-
-	/************************************************************************/
-	//	Methode uniquement utilisable dans les méthodes de la class DB 
-	//	permettant d'exécuter n'importe quelle requête SQL
-	//	et renvoyant en résultat les tuples renvoyés par la requête
-	//	sous forme d'un tableau d'objets
-	//	param1 : texte de la requête à exécuter (éventuellement paramétrée)
-	//	param2 : tableau des valeurs permettant d'instancier les paramètres de la requête
-	//	NB : si la requête n'est pas paramétrée alors ce paramètre doit valoir null.
-	//	param3 : nom de la classe devant être utilisée pour créer les objets qui vont
-	//	représenter les différents tuples.
-	//	NB : cette classe doit avoir des attributs qui portent le même que les attributs
-	//	de la requête exécutée.
-	//	ATTENTION : il doit y avoir autant de ? dans le texte de la requête
-	//	que d'éléments dans le tableau passé en second paramètre.
-	//	NB : si la requête ne renvoie aucun tuple alors la fonction renvoie un tableau vide
-	/************************************************************************/
-	private function execQuery($requete,$tparam,$nomClasse) {
-			//on prépare la requête
-		$stmt = $this->connect->prepare($requete);
-		//on indique que l'on va récupére les tuples sous forme d'objets instance de Client
-		$stmt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, $nomClasse); 
-		//on exécute la requête
-		if ($tparam != null) {
-			$stmt->execute($tparam);
-		}
-		else {
-			$stmt->execute();
-		}
-		//récupération du résultat de la requête sous forme d'un tableau d'objets
-		$tab = array();
-		$tuple = $stmt->fetch(); //on récupère le premier tuple sous forme d'objet
-		if ($tuple) {
-			//au moins un tuple a été renvoyé
-				while ($tuple != false) {
-			$tab[]=$tuple; //on ajoute l'objet en fin de tableau
-					$tuple = $stmt->fetch(); //on récupère un tuple sous la forme
-						//d'un objet instance de la classe $nomClasse	       
-			} //fin du while	           	     
-			}
-		return $tab;    
-	}
-
-	/************************************************************************/
-	//	Methode utilisable uniquement dans les méthodes de la classe DB
-	//	permettant d'exécuter n'importe quel ordre SQL (update, delete ou insert)
-	//	autre qu'une requête.
-	//	Résultat : nombre de tuples affectés par l'exécution de l'ordre SQL
-	//	param1 : texte de l'ordre SQL à exécuter (éventuellement paramétré)
-	//	param2 : tableau des valeurs permettant d'instancier les paramètres de l'ordre SQL
-	//	ATTENTION : il doit y avoir autant de ? dans le texte de la requête
-	//	que d'éléments dans le tableau passé en second paramètre.
-	/************************************************************************/
-	private function execMaj($ordreSQL,$tparam) {
-			$stmt = $this->connect->prepare($ordreSQL);
-		$res = $stmt->execute($tparam); //execution de l'ordre SQL      	     
-		return $stmt->rowCount();
-	}
-
-
-
-
-	/*************************************************************************
-	* Fonctions qui peuvent être utilisées dans les scripts PHP
-	*************************************************************************/
-
-	/*** METHODES POUR LES PRODUITS ***/
-
-	/** Récuperer les produits. Trier par categorie.
-	 * @return array tableau d'objets de la classe Produit
-	 */
-	public function getProduits() {
-		$requete = 'SELECT * FROM Produit ORDER BY categorie';
-		return $this->execQuery($requete,null,'Produit');
-	}
-
-	/** Récuperer les produits en fonction de la catégorie.
-	 * @param string $categ la catégorie des produits à récupérer
-	 * @return array tableau d'objets de la classe Produit
-	 */
-	public function getProduitsParCateg($categ) {
-		$requete = 'SELECT * FROM Produit WHERE categorie = ?';
-		return $this->execQuery($requete,array($categ),'Produit');
-	}
-
-	/** Modifier les données d'un produit.
-	 * @param Produit $produits le produit à modifier.
-	 */
-	public function updateProduit($produits) {
-		$requete = 'UPDATE Produit SET libProd = ?, prixUni = ?, categorie = ? WHERE idProd = ?';
-		$this->execQuery($requete,array($produits->getLibProd(),$produits->getPrixUni(),$produits->getCategorie(),$produits->getIdProd()),'Produit');
-	}
-
-	/**
-	 * Créer un produit.
-	 * @param Produit $produits le produit à créer.
-	 */
-	public function insertProduit($produits) {
-		$requete = 'INSERT INTO Produit VALUES (?,?,?,?)';
-		$this->execQuery($requete,array($produits->getIdProd(),$produits->getLibProd(),$produits->getPrixUni(),$produits->getCategorie()),'Produit');
-	}
-
-	/** Supprimer un produit.
-	 * @param Produit $produits le produit à supprimer
-	 */
-	public function suppProduit($produits) {
-		$requete = 'DELETE FROM Produit WHERE idProd = ?';
-		$this->execQuery($requete,array($produits->getIdProd()),'Produit');
-	}
-
-} //fin classe DB
-<?php
-
-//Mettre les objet a require ici /!\
 include_once 'Droit.inc.php';
 include_once 'Utilisateur.inc.php';
 include_once 'UtilisateurDroit.inc.php';
 include_once 'Client.inc.php';
+include_once 'Produit.inc.php';
 
 class DB {
 
@@ -336,6 +158,8 @@ class DB {
 		}
 		return $tab;
 	}
+
+	
 	/*** METHODES POUR LES UTILISATEURS ****/
 
 	/** Obtenir un utilisateur à partir de son login */
@@ -393,9 +217,6 @@ class DB {
 	}
 
 
-
-
-
 	/*** METHODES POUR LES CLIENTS ****/
 
 	/** Récuperer les clients. */
@@ -434,6 +255,49 @@ class DB {
 		return $this->execQuery($requete,array($client->getNomClub(),$client->getEmail(),$client->getTelephone(),$client->getPresent()),'Client');
 	}
 
+	/*** METHODES POUR LES PRODUITS ***/
+
+	/** Récuperer les produits. Trier par categorie.
+	 * @return array tableau d'objets de la classe Produit
+	 */
+	public function getProduits() {
+		$requete = 'SELECT * FROM Produit ORDER BY categorie';
+		return $this->execQuery($requete,null,'Produit');
+	}
+
+	/** Récuperer les produits en fonction de la catégorie.
+	 * @param string $categ la catégorie des produits à récupérer
+	 * @return array tableau d'objets de la classe Produit
+	 */
+	public function getProduitsParCateg($categ) {
+		$requete = 'SELECT * FROM Produit WHERE categorie = ?';
+		return $this->execQuery($requete,array($categ),'Produit');
+	}
+
+	/** Modifier les données d'un produit.
+	 * @param Produit $produits le produit à modifier.
+	 */
+	public function updateProduit($produits) {
+		$requete = 'UPDATE Produit SET libProd = ?, prixUni = ?, categorie = ? WHERE idProd = ?';
+		$this->execQuery($requete,array($produits->getLibProd(),$produits->getPrixUni(),$produits->getCategorie(),$produits->getIdProd()),'Produit');
+	}
+
+	/**
+	 * Créer un produit.
+	 * @param Produit $produits le produit à créer.
+	 */
+	public function insertProduit($produits) {
+		$requete = 'INSERT INTO Produit VALUES (?,?,?,?)';
+		$this->execQuery($requete,array($produits->getIdProd(),$produits->getLibProd(),$produits->getPrixUni(),$produits->getCategorie()),'Produit');
+	}
+
+	/** Supprimer un produit.
+	 * @param Produit $produits le produit à supprimer
+	 */
+	public function suppProduit($produits) {
+		$requete = 'DELETE FROM Produit WHERE idProd = ?';
+		$this->execQuery($requete,array($produits->getIdProd()),'Produit');
+	}
 
 	
-} //f
+} //fin classe DB
