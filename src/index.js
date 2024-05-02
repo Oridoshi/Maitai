8a7cc80d13c779d47e192ea4aba58ea0fcd125c8
MartinQueval <martin.queval@gmail.com>
2024-05-02 16:59:48 +0200

    push de fin de journée

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 // Style
+import './style/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './style/index.css';
 
 //Composants
 import Navbar from './components/Navbar';
--- src/pages/Tickets.js
+++ src/pages/Tickets.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import Table from '../components/Table';
+import Compteur from '../components/Compteur';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Ticket()
 {
-
+	const [modalOpen, setModalOpen] = useState(false);
+	const [lblCat, setLblCat] = useState();
+	const [lblProd, setLblProd] = useState();
 
 	// En-tête de la table
 	const initialHeader = [
@@ -17,10 +23,10 @@ export default function Ticket()
 
 	// En-tête de la table
 	const initData = [
-		{ id: '1', nomcli: 'test', email: 't', prix: 2, present: true },
-		{ id: '2', nomcli: 'test', email: 't', prix: 2, present: true },
-		{ id: '3', nomcli: 'test', email: 't', prix: 2, present: true },
-		{ id: '4', nomcli: 'test', email: 't', prix: 2, present: true },
+		{ id: '1', nomcli: 'test', email: 't', prix: 2 },
+		{ id: '2', nomcli: 'test', email: 't', prix: 2 },
+		{ id: '3', nomcli: 'test', email: 't', prix: 2 },
+		{ id: '4', nomcli: 'test', email: 't', prix: 2 },
 	];
 
 	// FILTRES
@@ -29,28 +35,86 @@ export default function Ticket()
 
 	function editTickets(ligne)
 	{
-		const ligneExist = document.getElementById('edit')
-		if(ligneExist){
+		const ligneExist = document.getElementById('edit');
+		if (ligneExist)
+		{
 			ligneExist.remove();
-		}
-		else{
+		} else
+		{
+			const Prod = [
+				{ nomP: 'Lampe', prix: 5, qt: 10 },
+				{ nomP: 'Combinaison', prix: 25, qt: 5 },
+				{ nomP: 'Lunette', prix: 10, qt: 2 },
+			];
 
-			//creation de la ligne avec un id edit pour la trouver et la masquer
-			const ligneEdit = document.createElement('tr');
+			// Création de la ligne avec un id edit pour la trouver et la masquer
+			const ligneEdit = document.createElement('div');
 			ligneEdit.id = 'edit';
-			ligneEdit.classList.add("col-sm-12");
+			ligneEdit.classList.add("ligneTicket");
 
-			initialHeader.forEach(column => {
-				const col = document.createElement('td');
-				col.textContent = ''; // Contenu vide par défaut
-				ligneEdit.appendChild(col);
+			//création du bouton d'ajout
+			const btnAdd = document.createElement('button');
+			btnAdd.classList.add('btnAjouter');
+			btnAdd.classList.add('btnProd');
+			btnAdd.classList.add('btn');
+			btnAdd.addEventListener('click', ajtProd);
+			ligneEdit.appendChild(btnAdd);
+
+
+			// Création des lignes de produit
+			Prod.forEach(prod =>
+			{
+				const ligneProd = document.createElement('div');
+				ligneProd.classList.add('ligneProd');
+
+				// Nom Produit
+				const nomP = document.createElement('div');
+				nomP.textContent = prod.nomP;
+
+				// Prix Produit
+				const prix = document.createElement('div');
+				prix.textContent = prod.prix + " €";
+
+				// Compteur
+				const compteur = document.createElement('div');
+				const compteurComponent = <Compteur valIni={ prod.qt } />;
+				ReactDOM.render(compteurComponent, compteur);
+
+				// Positionnement
+				ligneProd.appendChild(nomP);
+				ligneProd.appendChild(prix);
+				ligneProd.appendChild(compteur);
+
+				ligneEdit.appendChild(ligneProd); // Ajout de la ligne de produit à la table
 			});
 
-			//cherche le client dont on veut edit le ticket et ajoute la ligneEdit en dessous
+			// Création du bouton d'export
+			const btnExport = document.createElement('button');
+			btnExport.textContent = "Export (csv)";
+			btnExport.classList.add("btnExport");
+			btnExport.classList.add("btn");
+			btnExport.addEventListener('click', exportation);
+
+
+			// Ajout du bouton d'export à la ligne edit
+			ligneEdit.appendChild(btnExport);
+
+			// Cherche le client dont on veut éditer le ticket et ajoute la ligneEdit en dessous
 			const client = document.getElementById('ligne ' + ligne.id);
 			client.parentNode.insertBefore(ligneEdit, client.nextSibling);
 		}
 	}
+
+	function ajtProd()
+	{
+		setModalOpen(true);
+	}
+
+	function exportation()
+	{
+		console.log("exportation");
+	}
+
 
 	function filter(value)
 	{
@@ -78,6 +142,13 @@ export default function Ticket()
 		setFilterData(filteredData);*/
 	}
 
+	function activerProd(){
+		const btnProd = document.getElementById("btnProd");
+		btnProd.disabled=false;
+	}
+	function activerPrix(){
+		const prix = document.getElementById("prix");
+	}
 
 	return (
 		<div>
@@ -89,7 +160,7 @@ export default function Ticket()
 				</div>
 			</div>
 			<div className="form-check" style={ { marginLeft: '10em' } }>
-				<input type='checkbox' className="form-check-input border-secondary" id="afficherClients" onChange={ handleCbChange } />
+				<input type='checkbox' className="form-check-input border-secondary" id="afficherClients" onChange={()=> handleCbChange } />
 				<label className="form-check-label" htmlFor="afficherClients">Afficher seulement clients présents</label>
 			</div>
 			<div className='h-25'>
@@ -98,6 +169,43 @@ export default function Ticket()
 					data={ initData }
 				/>
 			</div>
+			<Modal show={ modalOpen } onHide={ () => setModalOpen(false) }>
+				<Modal.Header closeButton>
+					<Modal.Title>Ajouter un Produit</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div className="Modal">
+						<div className="dropdown itemModal">
+							<button className="btnProd btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Catégorie Produit
+							</button>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item"onClick={()=>{setLblCat("Matériel");activerProd()}}>Matériel</a></li>
+								<li><a className="dropdown-item"onClick={()=>{setLblCat("Consomation");activerProd()}}>Consomation</a></li>
+							</ul>
+						</div>
+						<label id="lblcat" className="lbl" >{lblCat} </label>
+						<div className="dropdown itemModal">
+							<button id="btnProd" className="btnProd btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" disabled>
+								Produit
+							</button>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" onClick={()=>{setLblProd("Lampe");activerPrix()}}>Lampe</a></li>
+								<li><a className="dropdown-item" onClick={()=>{setLblProd("Combinaison");activerPrix()}}>Combinaison</a></li>
+								<li><a className="dropdown-item" onClick={()=>{setLblProd("Lunette");activerPrix()}}>Lunette</a></li>
+							</ul>
+						</div>
+						<label id="lblprod" className="lbl"> {lblProd} </label>
+					</div>
+					<input id="prix" type="number" className="saisieprix" placeholder="Entrez un prix" disabled/>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button className="btnAjouter btn"></Button>
+					<Button variant="secondary" onClick={ () => setModalOpen(false) }>
+						Fermer
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	)
 }
