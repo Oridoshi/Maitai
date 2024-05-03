0c1819b0fb5b88655e7ae49bf628d68bed4e2b42
VicenteHugo <hugovicente965@gmail.com>
2024-05-03 12:56:43 +0200

    BHJDQ

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -5,11 +5,13 @@ import "../img/image.png"
 export default function accueil(){
 	return(
 		<div className="container">
-			<div class="row justify-content-start">
-				<h1 className="accueil col-sm-4">Maïtaï, Enfin une carrière à ma taille</h1>
-	
-				<img src={require("../img/image.png")} alt="carrière" className="img-fluid col-sm-8"/>
-			</div>		
-		</div>
+	<div className="row justify-content-start align-items-center">
+		<h1 className="accueil col-sm-4 ps-3">Maïtaï, Enfin une carrière à ma taille</h1>
+		<p className="col"></p>
+		<img src={require("../img/image.png")} alt="carrière" className="custom-img col" />
+	</div>
+	</div>
+
+
 	) 
 }
