5369247b5eca67c3db014497dea472a07114931f
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2024-05-04 00:41:37 +0200

    acceuil terminé

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap'; // Assurez-vous d'avoir installé react-bootstrap
+import image1 from "../img/image1.png";
+import image2 from "../img/image2.png";
+
+const ImageCarousel = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default ImageCarousel;
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,17 +1,32 @@
-import React from "react";
-import "../style/accueil.css"
-import "../img/image.png"
+	/* eslint-disable jsx-a11y/img-redundant-alt */
+	import React from "react";
+	import "../style/accueil.css"
+	import "../img/image.png"
+	import ImageCarousel from "../components/Carousel"
 
-export default function accueil(){
-	return(
-		<div className="container">
-	<div className="row justify-content-start align-items-center">
-		<h1 className="accueil col-sm-4 ps-3">Maïtaï, Enfin une carrière à ma taille</h1>
-		<p className="col"></p>
-		<img src={require("../img/image.png")} alt="carrière" className="custom-img col" />
-	</div>
-	</div>
+	export default function accueil(){
+		return(
+			<div className="container">
+				<div className="row justify-content-start align-items-center">
+					<p className="accueil col-sm-4 ps-3">Maïtaï, Enfin une carrière à ma taille</p>
+					<p className="col"></p>
+					<div className="col-md-4">
+						<ImageCarousel  />
+					</div>
+				</div>
+				
+				{/* <div className="container">
+					<div className="row">
+						<div className="col-md-4">
+							<img src={require("../img/image1.png")} className="img-fluid" alt="Image 1" />
+						</div>
+						<div className="col-md-4">
+							<img src={require("../img/image2.png")} className="img-fluid" alt="Image 2" />
+						</div>
+					</div>
+				</div> */}
+			</div>
 
 
-	) 
-}
+		) 
+	}
