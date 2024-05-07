ab5aeeda3a60f9615f6112ade2e790b3d62f1ad5
VicenteHugo <hugovicente965@gmail.com>
2024-05-07 17:04:27 +0200

    a*

--- src/php/other/historique/CreationHistorique.php
+++ src/php/other/historique/CreationHistorique.php
@@ -9,7 +9,7 @@ $file = $_FILES['file'];
 $fileName = $_POST['name'];
 $fileTmpPath = $file['tmp_name'];
 
-$chemin = "/" . $type . "/";
+$chemin = $_SERVER['DOCUMENT_ROOT'] ."/". $type . "/";
 //$chemin = "C:\\xampp\\htdocs\\historique\\" . $type . "\\";
 
 $val = count(DB::getInstance()->getHistoriques());
@@ -27,6 +27,7 @@ $historique = DB::getInstance()->getHistoriqueByChemin($chemin . $val . $fileNam
 
 if(!move_uploaded_file($fileTmpPath, $chemin . $val . $fileName)) {
     echo "Error moving file.";
+    echo $fileTmpPath;
     DB::getInstance()->suppHistorique($historique->getIdHis());
     return;
 }
