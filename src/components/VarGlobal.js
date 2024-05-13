834d926ade005f93638defebfd7f4eed9ca94084
Ottirate <sarah.hautot76@gmail.com>
2024-05-13 10:51:51 +0200

    Tableau et redirection

--- src/components/VarGlobal.js
+++ src/components/VarGlobal.js
@@ -1,5 +1,5 @@
-export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/"; //chemin vers le php
-//export const cheminPHP = "http://localhost/Maitai/src/php/other/";
+// export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/"; //chemin vers le php
+export const cheminPHP = "http://localhost/Maitai/src/php/other/";
 
 export const nivEncadrant = ['','E1','E2','E3','E4'];
 export const nivGeneral   = ['','Bapteme', 'N1-PE20','N2-PA20PE40','N3-PA60','PE60','E1','E2','E3','E4'];
--- src/php/inc/DB.inc.php
+++ src/php/inc/DB.inc.php
@@ -14,19 +14,19 @@ class DB {
 	private static $instance = null; //mémorisation de l'instance de DB pour appliquer le pattern Singleton
 	private $connect=null; //connexion PDO à la base
 
-	private static string $dbName   = "if0_36460769_maitai";
-	//private static string $dbName   = "maitai";
+	// private static string $dbName   = "if0_36460769_maitai";
+	private static string $dbName   = "maitai";
 
-	private static string $login    = "if0_36460769";
-	//private static string $login    = "Admin";
+	// private static string $login    = "if0_36460769";
+	private static string $login    = "Admin";
 
-	private static string $password = "Sc4ZKSO8sanWyvz";
-	//private static string $password = "maitai";
+	// private static string $password = "Sc4ZKSO8sanWyvz";
+	private static string $password = "maitai";
 
 	private static string $port     = "3306";
 
-	private static string $host     = "sql211.infinityfree.com";
-	//private static string $host     = "localhost";
+	// private static string $host     = "sql211.infinityfree.com";
+	private static string $host     = "localhost";
 
 
 	/************************************************************************/
