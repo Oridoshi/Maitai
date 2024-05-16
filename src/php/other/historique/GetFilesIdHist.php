27d37aa425dc375993fc15e2fd00665584e33f29
VicenteHugo <hugovicente965@gmail.com>
2024-05-16 15:25:40 +0200

    a

--- src/php/other/historique/GetFilesIdHist.php
+++ src/php/other/historique/GetFilesIdHist.php
@@ -2,7 +2,8 @@
 header("Access-Control-Allow-Origin: *");
 include_once '../../inc/DB.inc.php';
 
-$idhist = $_POST['idhist'];
+//$idhist = $_POST['idhist'];
+$idhist = 15;
 
 $chemin = DB::getInstance()->getHistoriquesById($idhist);
 
