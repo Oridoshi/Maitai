16566389795bd816d105dcff88ef5b88d7be0973
Ottirate <sarah.hautot76@gmail.com>
2024-05-17 13:45:09 +0200

    Script de création de la base pour la V2

---
+++ src/sql2/create.sql
@@ -0,0 +1,89 @@
+-- Suppression des tables de premier niveau de liaison
+DROP TABLE IF EXISTS UtilisateurDroit;
+DROP TABLE IF EXISTS Historique      ;
+DROP TABLE IF EXISTS Ticket          ;
+
+-- Suppression des tables non-lié.
+DROP TABLE IF EXISTS Utilisateur;
+DROP TABLE IF EXISTS Produit    ;
+
+
+
+
+/*******************************************/
+/**                                       **/
+/**               LIAISON 0               **/
+/**                                       **/
+/*******************************************/
+
+-- Création de la table utilisateur
+CREATE TABLE Utilisateur
+(
+	-- Utilisateur
+	idUti SERIAL      PRIMARY KEY       ,
+	login VARCHAR(20) NOT NULL UNIQUE   ,
+	email VARCHAR(60) NOT NULL UNIQUE   ,
+	mdp   VARCHAR(20) NOT NULL          ,
+	actif BOOLEAN     NOT NULL DEFAULT 1,
+
+	-- Partie client
+	telephone VARCHAR(10)          ,
+	present   BOOLEAN     DEFAULT 1,
+
+	-- Partie droit : Seulement trois possible; 'Client, Maitai, Admin' 
+	droit     VARCHAR(6) NOT NULL CHECK (type IN ('Client', 'Maitai', 'Admin')),
+);
+
+
+-- Insertion d'un utilisateur admin
+INSERT INTO Utilisateur (login, email, mdp, droit) VALUE ('admin1', 'votre.addresse@gmail.fr', 'mdp1', 'Admin');
+
+
+-- Création de la table produit
+CREATE TABLE Produit
+(
+	idProd    INTEGER       PRIMARY KEY,
+	libProd   VARCHAR(255)  NOT NULL   ,
+	prixUni   DECIMAL(12,2)            ,
+	categorie VARCHAR(30)   NOT NULL
+);
+
+
+
+
+/*******************************************/
+/**                                       **/
+/**               LIAISON 1               **/
+/**                                       **/
+/*******************************************/
+
+-- Création de la table historique : permet de garder en mémoire les chemins menant au fichier sauvegarder sur le serveur.
+CREATE TABLE Historique
+(
+	idHis  SERIAL       PRIMARY KEY,
+	date   DATE         DEFAULT CURRENT_DATE NOT NULL,
+	chemin VARCHAR(255) NOT NULL,
+	type   VARCHAR(6)   NOT NULL CHECK (type IN ('TICKET', 'SECU')),
+	idUti  INTEGER      NOT NULL REFERENCES Utilisateur(idUti)
+);
+
+-- Création de la table ticket : permet de garder en mémoire la commande actuellement en cours des gens
+CREATE TABLE Ticket
+(
+	idProd  INTEGER REFERENCES Produit(idProd),
+	idUti   INTEGER REFERENCES Utilisateur(idUti),
+	qa      INTEGER NOT NULL CHECK (qa >0),
+	prixTot DECIMAL(12,2) DEFAULT NULL,
+	PRIMARY KEY (idProd, idUti)
+);
+
+-- Création de la table demande : permet de savoir les demandes de clients pour un jour précis (permet de prévoir le stock)
+CREATE TABLE Demande
+(
+	idProd   INTEGER REFERENCES Produit(idProd),
+	idUti    INTEGER REFERENCES Utilisateur(idUti),
+	qa       INTEGER NOT NULL CHECK (qa >0),
+	date     DATE    NOT NULL,
+	pourMidi BOOLEAN NOT NULL,
+	PRIMARY KEY (idProd, idUti)
+);
