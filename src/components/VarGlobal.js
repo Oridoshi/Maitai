c6cb126e722a677063eb171123132b71e4248a68
Richard-becon <rich.metayer@gmail.com>
2024-05-30 09:07:53 +0200

    push

--- src/components/VarGlobal.js
+++ src/components/VarGlobal.js
@@ -1,6 +1,6 @@
-export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/";
+//export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/";
 // export const cheminPHP = "http://172.26.4.207/Maitai/src/php/other/";
-// export const cheminPHP = "http://localhost/Maitai/src/php/other/";
+export const cheminPHP = "http://localhost/Maitai/src/php/other/";
 
 export const nivEncadrant = ['', 'E1', 'E2', 'E3', 'E4', 'N4-GP'];
 export const nivGeneral   = ['','Bapteme', 'N1-PE20','N2-PA20PE40','N3-PA60','PE60','E1','E2','E3','E4','N4-GP'];
