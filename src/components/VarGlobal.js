412302eb768d4ae03b20924d60d3284c0c1e1a3c
MartinQueval <martin.queval@gmail.com>
2024-06-14 14:20:29 +0200

    push

--- src/components/VarGlobal.js
+++ src/components/VarGlobal.js
@@ -1,7 +1,7 @@
-export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/";
+//export const cheminPHP = "https://maitai-becon.wuaze.com/php/other/";
 //export const cheminPHP = "http://172.26.4.207/Maitai/src/php/other/";
 //export const cheminPHP = "http://192.168.1.65/Maitai/src/php/other/";
-//export const cheminPHP = "http://192.168.195.64/Maitai/src/php/other/";
+export const cheminPHP = "http://192.168.195.64/Maitai/src/php/other/";
 // export const cheminPHP = "http://localhost/Maitai/src/php/other/";
 
 export const nivEncadrant = ['', 'E1', 'E2', 'E3', 'E4', 'N4-GP'];
