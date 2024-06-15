abf4f3fbfca75ff7021a1d456f2afe60c94988a7
hugo vicente <hugovicente965@gmail.com>
2024-06-15 10:51:52 +0200

    normalement c'est fini

---
+++ src/php/other/palanquee/CreationPalanquee.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+include_once '../../inc/DB.inc.php';
+
+$pdo = DB::getInstance();
+
+$palanquee = new Palanquee();
+
+$palanquee->setNomPlongeurs($_POST['nomPlongeurs']);
+$palanquee->setHd($_POST['hd']);
+$palanquee->setDuree($_POST['duree']);
+
+$pdo->insertPalanquee($palanquee);
+
+echo $pdo->getCurrentIdPalanquee();
